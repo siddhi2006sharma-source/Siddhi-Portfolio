e83c86c0a5758cb48aa2529710916ce616b409ed
siddhi2006sharma-source <siddhi2006sharma@gmail.com>
2026-09-23 15:34:15 -0400

    Update script.js

--- Rock Paper Scissors Game/script.js
+++ Rock Paper Scissors Game/script.js
@@ -1,1 +1,69 @@
+let userScore=0;
+let compScore=0;
+const choices = document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+const userScorePara=document.querySelector("#user-score");
+const compScorePara=document.querySelector("#comp-score");
 
+
+const genCompChoice=()=>{
+const options =["rock","paper","scissors"];
+const randIdx=Math.floor(Math.random()*3);
+return options[randIdx];
+};
+
+
+const drawGame =()=> {
+console.log("game was draw");
+msg.innerText="Game was Draw. Play Again. ";
+  msg.style.backgroundColor ="rgb(16, 16, 67)";
+}
+
+
+const showWinner=(userWin , userChoice,compChoice)=> {
+if(userWin){
+    userScore++;
+    userScorePara.innerText=userScore;
+    msg.innerText=`You Win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor ="green";
+
+}else{
+    compScore++;
+    compScorePara.innerText=compScore;
+    msg.innerText=`You Lose! ${compChoice} beats your ${userChoice} `;
+      msg.style.backgroundColor ="red";
+}
+}
+const playGame =(userChoice)=>{
+console.log("userChoice is =", userChoice);
+//generate computer choice 
+const compChoice =genCompChoice();
+console.log("compChoice is =", compChoice);
+
+
+if(userChoice===compChoice){
+    //Draw Game 
+    drawGame();
+}else{
+    let userWin=true;
+    if(userChoice==="rock"){
+        //paper,scissors
+       userWin= compChoice==="paper"? false :true;
+    }else if(userChoice==="paper"){
+        //rock,scissors 
+        userWin= compChoice==="scissors"? false:true;
+    }else{
+        //rock,paper
+        userWin= compChoice==="rock"? false:true;
+    }
+showWinner(userWin,userChoice,compChoice);
+}
+};
+
+
+choices.forEach((choice) => {
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
