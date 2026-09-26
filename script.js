a9ee8a941e233057a2da84985224937939ba9ec3
siddhi2006sharma-source <siddhi2006sharma@gmail.com>
2026-09-26 05:50:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const menuBtn = document.getElementById("menuBtn");
 const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", function () {
-
     navLinks.classList.toggle("active");
-
 });
 
 
@@ -17,29 +15,7 @@ const links = document.querySelectorAll(".nav-links a");
 links.forEach(function (link) {
 
     link.addEventListener("click", function () {
-
         navLinks.classList.remove("active");
-
     });
-
-});
-
-
-// ================= CONTACT FORM =================
-
-const contactForm = document.getElementById("contactForm");
-
-contactForm.addEventListener("submit", function (event) {
-
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
-
-    alert(
-        "Thank you, " + name +
-        "! Your message has been received."
-    );
-
-    contactForm.reset();
 
 });
